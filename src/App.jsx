53c4c549fe7caca1ce816a51390f8edc5a43008b
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-15 09:58:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='w-screen h-screen p-10 bg-gray-100 text-slate-700'>
+      <h1 className='max-w-md mx-auto text-2xl text-center font-bold'>
+        Simple Counter Application
+      </h1>
+
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+
+      </div>
+    </div>
+  )
+}
+e
+export default App
