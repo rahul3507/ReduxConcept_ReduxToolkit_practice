d83076b0a2bfba32502ceb5a70d20dc418d44beb
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-17 12:20:05 +0600

    Refactor extraReducers in postsSlice for improved readability and consistency

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -17,19 +17,20 @@ export const postsSlice = createSlice({
     name: 'posts',
     initialState,
     extraReducers: (builder)=> {
-        builder.addCase(fetchPosts.pending, (state)=>{
-            state.isError = false;
-            state.isLoading=true;
-        });
-        builder.addCase(fetchPosts.fulfilled, (state, action)=>{
-            state.isLoading=false;
-            state.posts = action.payload
-        });
-        builder.addCase(fetchPosts.rejected, ( state, action)=>{
-            state.isLoading = false;
-            state.isError=true;
-            state.error= action.error?.message;
-        })
+        builder
+            .addCase(fetchPosts.pending, (state)=>{
+                state.isError = false;
+                state.isLoading=true;
+            })
+            .addCase(fetchPosts.fulfilled, (state, action)=>{
+                state.isLoading=false;
+                state.posts = action.payload
+            })
+            .addCase(fetchPosts.rejected, ( state, action)=>{
+                state.isLoading = false;
+                state.isError=true;
+                state.error= action.error?.message;
+            })
 
     }
 })
